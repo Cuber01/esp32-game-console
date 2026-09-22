ece157947bab66ecb86b0a44bc5833492ffd186c
Cuber01 <johnmallard631@gmail.com>
2026-09-22 13:53:10 +0200

    Commented out old adafruit library code

--- include/graphics.h
+++ include/graphics.h
@@ -5,6 +5,8 @@
 #define SCREEN_CS        5
 #define SCREEN_RST       16
 #define SCREEN_DC        17
+#define SCREEN_SCL       18
+#define SCREEN_SDA       23
 
 #define SCREEN_WIDTH 240
 #define SCREEN_HEIGHT 320
--- src/graphics.cpp
+++ src/graphics.cpp
@@ -1,7 +1,5 @@
 
 #include "graphics.h"
-#include <Adafruit_GFX.h>
-#include <Adafruit_ST7789.h>
 
 #include "apples.h"
 #include "game.h"
@@ -12,29 +10,29 @@
 // SCLK = pin 13. This is the fastest mode of operation and is required if
 // using the breakout board's microSD card.
 
-Adafruit_ST7789 tft = Adafruit_ST7789(SCREEN_CS, SCREEN_DC, SCREEN_RST);
+// Adafruit_ST7789 tft = Adafruit_ST7789(SCREEN_CS, SCREEN_DC, SCREEN_RST);
 
 void init() {
-    tft.init(SCREEN_WIDTH, SCREEN_HEIGHT);
-    tft.fillScreen(0);
+    //tft.init(SCREEN_WIDTH, SCREEN_HEIGHT);
+    //tft.fillScreen(0);
     //tft.setSPISpeed(40000000);
 }
 
 void appleCounterDraw() {
-    tft.setCursor(0, 0);
-    tft.setTextColor(ST77XX_YELLOW, ST77XX_BLACK);
-    tft.setTextWrap(false);
-    tft.print("apples: ");
+    // tft.setCursor(0, 0);
+    // tft.setTextColor(ST77XX_YELLOW, ST77XX_BLACK);
+    // tft.setTextWrap(false);
+    // tft.print("apples: ");
 
     // Clear last number
-    int16_t lastX = tft.getCursorX();
-    int16_t lastY = tft.getCursorY();
-    tft.setTextColor(ST77XX_MAGENTA, ST77XX_BLACK);
-    tft.print("      ");
-
-    tft.setCursor(lastX, lastY);
-    tft.setTextColor(ST77XX_YELLOW, ST77XX_BLACK);
-    tft.print(ApplesEaten);
+    // int16_t lastX = tft.getCursorX();
+    // int16_t lastY = tft.getCursorY();
+    // tft.setTextColor(ST77XX_MAGENTA, ST77XX_BLACK);
+    // tft.print("      ");
+    //
+    // tft.setCursor(lastX, lastY);
+    // tft.setTextColor(ST77XX_YELLOW, ST77XX_BLACK);
+    // tft.print(ApplesEaten);
     //ESP.getFreeHeap()
 }
 
@@ -44,11 +42,11 @@ void mapDraw() {
         for (int x = 0; x < MAP_WIDTH; x++)
         {
             if (mapGet(x,y) == APPLE_SYMBOL) {
-                tft.fillRect(x*MAP_BLOCK_SIZE, y*MAP_BLOCK_SIZE, MAP_BLOCK_SIZE, MAP_BLOCK_SIZE, ST77XX_RED);
+                //tft.fillRect(x*MAP_BLOCK_SIZE, y*MAP_BLOCK_SIZE, MAP_BLOCK_SIZE, MAP_BLOCK_SIZE, ST77XX_RED);
             } else if (mapGet(x,y) == SNAKE_SYMBOL) {
-                tft.fillRect(x*MAP_BLOCK_SIZE, y*MAP_BLOCK_SIZE, MAP_BLOCK_SIZE, MAP_BLOCK_SIZE, ST77XX_GREEN);
+                //tft.fillRect(x*MAP_BLOCK_SIZE, y*MAP_BLOCK_SIZE, MAP_BLOCK_SIZE, MAP_BLOCK_SIZE, ST77XX_GREEN);
             } else if (mapGet(x,y) == GROUND_SYMBOL) {
-                tft.fillRect(x*MAP_BLOCK_SIZE, y*MAP_BLOCK_SIZE, MAP_BLOCK_SIZE, MAP_BLOCK_SIZE, ST77XX_WHITE);
+                //tft.fillRect(x*MAP_BLOCK_SIZE, y*MAP_BLOCK_SIZE, MAP_BLOCK_SIZE, MAP_BLOCK_SIZE, ST77XX_WHITE);
             }
         }
     }
@@ -64,7 +62,7 @@ void graphicsDemo() {
     static int X = 0;
     static int Y = 0;
 
-    tft.fillRect(X*MAP_BLOCK_SIZE,Y*MAP_BLOCK_SIZE,MAP_BLOCK_SIZE, MAP_BLOCK_SIZE, ST77XX_WHITE);
+    //tft.fillRect(X*MAP_BLOCK_SIZE,Y*MAP_BLOCK_SIZE,MAP_BLOCK_SIZE, MAP_BLOCK_SIZE, ST77XX_WHITE);
     if (X >= MAP_WIDTH) {
         X = 0;
         Y += 1;
@@ -72,8 +70,8 @@ void graphicsDemo() {
         X += 1;
     }
 
-    tft.fillRect(MAP_WIDTH*8 -8, MAP_HEIGHT*8 -8, MAP_BLOCK_SIZE, MAP_BLOCK_SIZE, ST77XX_CYAN);
-    tft.fillRect(MAP_WIDTH*8 -8, 0, MAP_BLOCK_SIZE, MAP_BLOCK_SIZE, ST77XX_YELLOW);
-    tft.fillRect(0, MAP_HEIGHT*8 -8, MAP_BLOCK_SIZE, MAP_BLOCK_SIZE, ST77XX_MAGENTA);
-    tft.fillRect(0, 0, MAP_BLOCK_SIZE, MAP_BLOCK_SIZE, ST77XX_ORANGE);
+    // tft.fillRect(MAP_WIDTH*8 -8, MAP_HEIGHT*8 -8, MAP_BLOCK_SIZE, MAP_BLOCK_SIZE, ST77XX_CYAN);
+    // tft.fillRect(MAP_WIDTH*8 -8, 0, MAP_BLOCK_SIZE, MAP_BLOCK_SIZE, ST77XX_YELLOW);
+    // tft.fillRect(0, MAP_HEIGHT*8 -8, MAP_BLOCK_SIZE, MAP_BLOCK_SIZE, ST77XX_MAGENTA);
+    // tft.fillRect(0, 0, MAP_BLOCK_SIZE, MAP_BLOCK_SIZE, ST77XX_ORANGE);
 }
